7fd5e4cc9f6ecf2c9e2c3b26ec6ee0195b1c5239
Administrator <Administrator@222.112.124.239>
2020-11-11 17:43:06 +0900

    구구단 반복문

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hi world");
+
+		while (true) {
+			
+			Scanner scanner= new Scanner(System.in);	
+			int num=scanner.nextInt();
+			
+			
+			if (num >= 2 && num <= 9) {
+				for(int i=1;i<10;i++)
+				{
+					System.out.println(num * i);
+				}
+				break;
+			}
+
+			else {
+				System.out.println("다시 입력");
+			}
+
+		}
 	}
 
 }
